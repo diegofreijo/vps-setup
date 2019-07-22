5b5e1e4665ea1af470130dbfc91d4fb616df8995
Diego Freijo <giga.freijo@gmail.com>
2019-07-22 13:24:50 -0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -14,7 +14,7 @@ print "VPS setup starting!"
 export DEBIAN_FRONTEND=noninteractive
 apt update
 apt upgrade -y -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" --force-yes
-apt install -y golang-go autojump zsh python-pip python3-pip snapd nmap masscan
+apt install -y golang-go autojump zsh python-pip python3-pip snapd nmap masscan unzip
 
 # oh-my-zsh
 curl -Lo install.sh https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh
