569cf542632ef685b28ebcf6178f638270a5cc80
Diego Freijo <giga.freijo@gmail.com>
2019-08-01 00:41:51 -0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -14,7 +14,7 @@ print "VPS setup starting!"
 export DEBIAN_FRONTEND=noninteractive
 apt update
 apt upgrade -y -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" --force-yes
-apt install -y golang-go autojump zsh python-pip python3-pip snapd nmap masscan unzip sslscan nikto
+apt install -y golang-go autojump zsh python-pip python3-pip snapd nmap masscan unzip sslscan nikto dirb
 
 # oh-my-zsh
 curl -Lo install.sh https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh
@@ -23,14 +23,14 @@ cp .zshrc ~/.zshrc
 chsh -s $(which zsh)
 source ~/.zshrc
 
-# Amass
-snap install amass
+
 
 # Tools
 mkdir -p ~/tools
 cd ~/tools
 
-git clone https://github.com/j3ssie/Osmedeus && cd Osmedeus && ./install.sh
+snap install amass
+#git clone https://github.com/j3ssie/Osmedeus && cd Osmedeus && ./install.sh
 git clone https://github.com/danielmiessler/SecLists.git
 
 go get -u github.com/tomnomnom/assetfinder
