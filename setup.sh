64affebd682de578af4c757f81c874474d889cdf
Diego Freijo <giga.freijo@gmail.com>
2020-05-23 22:24:22 -0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -13,8 +13,8 @@ print "VPS setup starting!"
 
 export DEBIAN_FRONTEND=noninteractive
 apt update
-apt upgrade -y -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" --force-yes
-apt install -y golang-go autojump zsh python-pip python3-pip snapd nmap masscan unzip sslscan nikto dirb
+apt upgrade -y -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold"
+apt install -y golang-go autojump zsh python3-pip snapd nmap masscan unzip nikto dirb
 
 # oh-my-zsh
 curl -Lo install.sh https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh
@@ -22,7 +22,6 @@ sh install.sh --unattended
 cp .zshrc ~/.zshrc
 chsh -s $(which zsh)
 source ~/.zshrc
-
 
 
 # Tools
